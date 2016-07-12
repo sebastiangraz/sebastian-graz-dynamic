6aa0cd5f6ba59f48eba2c80612dedfae780318c6
Sebastian Graz <sebastian@Sebastians-MacBook-Pro-2.local>
2016-07-12 23:58:04 +0100

    v0.1

--- source/javascripts/smoothstate-init.js
+++ source/javascripts/smoothstate-init.js
@@ -69,7 +69,7 @@ var app = {};
                   duration: 100,    // the scene should last for a scroll distance of 100px
                   offset: 50        // start this scene after scrolling for 50px
               })
-              .setPin("#my") // pins the element for the the scene's duration
+              .setPin(".hero-computer") // pins the element for the the scene's duration
               .addTo(controller); // assign the scene to the controller
         }
     };
