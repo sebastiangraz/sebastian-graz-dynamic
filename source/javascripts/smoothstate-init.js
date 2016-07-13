c2fa07503a095eae1f083662f8f6f52e23fcb5d4
Sebastian Graz <sebastian@Sebastians-MacBook-Pro-2.local>
2016-07-13 20:50:47 +0100

    first version of the case grid v0.3

--- source/javascripts/smoothstate-init.js
+++ source/javascripts/smoothstate-init.js
@@ -66,7 +66,7 @@ var app = {};
           var controller = new ScrollMagic.Controller();
           // create a scene
           new ScrollMagic.Scene({
-                  duration: 100,    // the scene should last for a scroll distance of 100px
+                  duration: 200,    // the scene should last for a scroll distance of 100px
                   offset: 50        // start this scene after scrolling for 50px
               })
               .setPin(".hero-computer") // pins the element for the the scene's duration
