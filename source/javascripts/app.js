7b8e9ccb631fb7ff3ccb3aee3be50f21dc5434a2
Sebastian Graz <sebastian@Sebastians-MacBook-Pro-2.local>
2016-09-10 19:04:24 +0100

    better logo image, cv copy update

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -129,7 +129,7 @@ var app = {};
     },
     weather: function () {
       // https://github.com/AryanJ-NYC/local-weather/blob/gh-pages/weather.js#L6
-      const DARKSKY_APIKEY = 'd8044f3e42338203b270b3e6f2d4f83b';
+      const DARKSKY_APIKEY = '42c0b99f752f85b715fe11d76658f0e4';
       const LATITUDE = '51.52';
       const LONGITUDE = '0.08';
       const DARKSKY_URL = `https://api.forecast.io/forecast/${DARKSKY_APIKEY}/${LATITUDE},${LONGITUDE}`;
@@ -158,6 +158,9 @@ var app = {};
                 $(weatherIcon).html('☔');
         }
       }, "jsonp");
+      if('caches' in window) {
+        console.log('supported')
+      }
     }
   };
 
